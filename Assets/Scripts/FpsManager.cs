0dcbefc1ad61fefe06bc9a98bb8333a8bb1134db
R1nge <r1ngegamedev@gmail.com>
2023-08-07 09:37:14 +0300

    Set 60 fps android

---
+++ Assets/Scripts/FpsManager.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+public sealed class FpsManager
+{
+    [RuntimeInitializeOnLoadMethod]
+    static void SetFps()
+    {
+        QualitySettings.vSyncCount = 0;
+        Application.targetFrameRate = 61;
+    }
+}
